89f0d5983df5627f3e7f49f2cd40ff01f55616c2
CiCi5355 <96011047+CiCi5355@users.noreply.github.com>
2023-05-08 13:47:40 +0800

    Add files via upload

---
+++ command_maker_js/setting.js
@@ -0,0 +1,51 @@
+const data = {
+    title: "MCPE 指令生成器",
+    subtitle: "Make By CiCilove",
+    subtitles: ["(´◓Д◔`)", "(*´∀`)~♥", "(´-ι_-｀)",
+                "Σヽ(ﾟД ﾟ; )ﾉ", "(☍﹏⁰。)", "(ﾟ3ﾟ)～♪",
+                "(΄◞ิ౪◟ิ‵)", "（ﾟДﾟ）σ弌弌弌弌弌弌弌弌弌弌弌弌弌弌弌弌弌弌弌弌⊃"
+    ]
+}
+
+let lastNumber = 1;
+
+let titles = data.title.split('');
+
+let titleRunNumber = 0;
+
+function loaded(){
+    // document.getElementById("title").innerHTML = data.title;
+    // document.getElementById("subtitle").innerHTML = data.subtitle + "     (*´∀`)~♥";
+
+    //廣告板html生成
+    let ad_board_html = "";
+    for(let a = 0;a < titles.length; a++){
+        ad_board_html += `<h1 class="Noto-Sans-TC" id="title${a}" style="display: inline;">${titles[a]}</h1>`;
+    }
+    ad_board_html += `<p class="lead Noto-Sans-TC" id="subtitle">${data.subtitle + "     (*´∀`)~♥"}</p>`;
+    document.getElementById("ad_board").innerHTML = ad_board_html;
+
+    //廣告板主標題動態
+    const titleRun = window.setInterval(() => {
+        for(let a = 0;a < titles.length;a++){
+            if(a == titleRunNumber){
+                document.getElementById(`title${a}`).style.color = "rgb(255, 0 ,0)";
+            }else{
+                document.getElementById(`title${a}`).style.color = "rgb(0, 0 ,0)";
+            }
+        }
+        titleRunNumber++;
+        if(titleRunNumber > (titles.length - 1)) titleRunNumber = 0;
+    }, 100)
+
+    //廣告板副標題動態
+    const subtitleRun = window.setInterval(() => {
+        let number = 0;
+        do{
+            number = Math.floor(Math.random() * data.subtitles.length);
+        }while(number == lastNumber)
+        lastNumber = number;
+        let text = data.subtitle + "     " + data.subtitles[number];
+        document.getElementById("subtitle").innerHTML =  text;
+    }, 1000)
+}
